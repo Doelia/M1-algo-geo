5283e9e3a4049ba2e050690cbfe28e90eae84dcc
Doelia <kyxalia@gmail.com>
2015-04-27 15:51:46 +0200

    avancement

--- TP3/main.h
+++ TP3/main.h
@@ -470,6 +470,65 @@ void alphaShape(float alpha, std::vector<Triangle*> triangles) {
   }
 }
 
+struct pointTest {
+  int abscisse;
+  int ordonnee;
+};
+
+
+void loadTest() {
+  int n = 20;
+  pointTest sommet[n];
+
+  sommet[0].abscisse=123;
+  sommet[0].ordonnee=523;
+  sommet[1].abscisse=259;
+  sommet[1].ordonnee=476;
+  sommet[2].abscisse=411;
+  sommet[2].ordonnee=280;
+  sommet[3].abscisse=60;
+  sommet[3].ordonnee=400;
+  sommet[4].abscisse=187;
+  sommet[4].ordonnee=544;
+  sommet[5].abscisse=447;
+  sommet[5].ordonnee=381;
+  sommet[6].abscisse=296;
+  sommet[6].ordonnee=194;
+  sommet[7].abscisse=273;
+  sommet[7].ordonnee=614;
+  sommet[8].abscisse=130;
+  sommet[8].ordonnee=250;
+  sommet[9].abscisse=212;
+  sommet[9].ordonnee=587;
+  sommet[10].abscisse=500;
+  sommet[10].ordonnee=400;
+  sommet[11].abscisse=280;
+  sommet[11].ordonnee=170;
+  sommet[12].abscisse=280;
+  sommet[12].ordonnee=630;
+  sommet[13].abscisse=311;
+  sommet[13].ordonnee=233;
+  sommet[14].abscisse=345;
+  sommet[14].ordonnee=512;
+  sommet[15].abscisse=130;
+  sommet[15].ordonnee=550;
+  sommet[16].abscisse=430;
+  sommet[16].ordonnee=250;
+  sommet[17].abscisse=212;
+  sommet[17].ordonnee=587;
+  sommet[18].abscisse=430;
+  sommet[18].ordonnee=550;
+  sommet[19].abscisse=431;
+  sommet[19].ordonnee=450;
+
+  for (int i = 0; i < n; i++) {
+    points.push_back(new Point(sommet[i].abscisse, sommet[i].ordonnee, 0));
+  }
+
+
+}
+
+
 
 int ok = 0;
 vector<Triangle*> triangles;
@@ -481,8 +540,8 @@ void tp3() {
 
   if (ok == 0) {
     cout << "Génération..." << endl;
-    int n = 200;
-    generer(n);
+    int n = 20;
+    loadTest();
     trierWithAbssice();
     Point::displayAll(vectorToTab(points), n, false);
     triangles = triangulation();
@@ -503,6 +562,10 @@ void tp3() {
 
   ok++;
 
+  
+}
+
+void ex4() {
   
 }
 
@@ -555,8 +618,8 @@ void tp4(int alpha, bool ashape) {
 
 void exec(int alpha, bool ashape) {
 
-  //tp3();
-  tp4(alpha, ashape);
+  tp3();
+  //tp4(alpha, ashape);
 
 }
 
